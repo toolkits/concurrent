a4371d70e3e308db0d5f90c2b1f5efaa84add725
niean <niean.sail@gmail.com>
2015-06-24 12:00:57 +0000

    add sema.TryAcquire(), make sema concurrent safe

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -1,4 +1,4 @@
-package concurrent
+package semaphore
 
 import ()
 
@@ -11,11 +11,19 @@ func NewSemaphore(concurrencyNum int) *Semaphore {
 	return &Semaphore{channel: make(chan int8, concurrencyNum), bufSize: concurrencyNum}
 }
 
+func (this *Semaphore) TryAcquire() bool {
+	select {
+	case this.channel <- int8(0):
+		return true
+	default:
+		return false
+	}
+}
+
 func (this *Semaphore) Acquire() {
 	this.channel <- int8(0)
 }
 
-// Release前 必须Acquire
 func (this *Semaphore) Release() {
 	<-this.channel
 }
